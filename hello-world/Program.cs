c3e38c72b0140a30e89732daa888a9c1eed54fd3
CristianBanan <cristian.ursache96@yahoo.com>
2017-07-10 12:19:52 +0300

    exclamarii

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -10,7 +10,7 @@ namespace hello_world
     {
         static void Main(string[] args)
         {
-            Console.Out.Write("Hello");
+            Console.Out.Write("Hello!!!");
             Console.ReadKey();
         }
     }
